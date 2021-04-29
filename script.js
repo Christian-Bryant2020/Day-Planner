c4ab44e8ab313ffba82b53155c8fd9d56bbd0eff
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-04-28 19:27:09 -0500

    Displays current date and time

---
+++ script.js
@@ -0,0 +1,9 @@
+const currentDay = dayjs().format('ddd, MMMM D YYYY');
+const date = document.getElementById('todayDate');
+date.innerText = currentDay;
+
+function display() {
+
+dayjs() = date;
+console.log(success);
+};
