e83c1344964d52dd014caff3341686624cd7523f
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-06-03 18:28:20 -0500

    Colorize by hour

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 const currentDay = dayjs().format('ddd, MMMM D YYYY');
 const date = document.getElementById('todayDate');
-const hour =  dayjs().format("H");
+const hour =  12//dayjs().format("H");
 console.log(hour)
 
 date.innerText = currentDay;
 
-$(document).ready(function(){
+$(document).ready(function () {
+    let row = $(".row")
     var span = $(".span")
     var text = $("textarea")
     var saveButton = $("saveBtn")
-console.log(span)
-console.log(text)
-console.log(saveButton)
+    console.log(saveButton)
 
-});
+    //save to local storage text
+$('#saveBtn').click(function() {
+    console.log('click')
+         var value = $(this).parent().children("textarea").val()
+         var key = $(this).parent().attr("data-value")
+    
+         localStorage.setItem(buttonClickId + "hour", buttonClickText)
+    });
+
+    //color change dependent on time
+    row.each(function () {
+        var hourBlock = parseInt(this.dataset.value)
+        console.log(this.dataset.value)
+        if (hourBlock < hour) {
+            $(this).addClass("past")
+        }
+        else if (hourBlock === hour) {
+            $(this).addClass("present")
+        }
+        else {
+            $(this).addClass("future")
+        }
+    })
+
 
 //display local storage text
 function displayItems() {
     let value = localStorage.getItem(key);
-    if(value) {
+    if (value) {
         $(`#text${key}`).text(value);
     }
-};
+}});
+
