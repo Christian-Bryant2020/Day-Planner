7bbe21bea8f1dd51d11924c0f2b60a58666b566c
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-06-07 12:27:01 -0500

    Fixed set local storage

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Day-Planner
 Plan your workday with local storage
+
+![Landing page showing start button.](./Assets/Workday.PNG)
--- script.js
+++ script.js
@@ -1,47 +1,51 @@
 const currentDay = dayjs().format('ddd, MMMM D YYYY');
 const date = document.getElementById('todayDate');
-const hour =  12//dayjs().format("H");
-console.log(hour)
+const hour = dayjs().format("H");
 
 date.innerText = currentDay;
 
 $(document).ready(function () {
     let row = $(".row")
-    var span = $(".span")
-    var text = $("textarea")
-    var saveButton = $("saveBtn")
-    console.log(saveButton)
+    let span = $(".span")
+    let text = $("textarea")
+    let saveButton = $(".saveBtn")
 
     //save to local storage text
-$('#saveBtn').click(function() {
-    console.log('click')
-         var value = $(this).parent().children("textarea").val()
-         var key = $(this).parent().attr("data-value")
-    
-         localStorage.setItem(buttonClickId + "hour", buttonClickText)
+$(saveButton).click(function() {
+    let value = $(this).parent().children(".textarea").val()
+    let key = $(this).parent().attr("data-value")
+         localStorage.setItem(key , value)
     });
 
-    //color change dependent on time
-    row.each(function () {
-        var hourBlock = parseInt(this.dataset.value)
-        console.log(this.dataset.value)
-        if (hourBlock < hour) {
-            $(this).addClass("past")
-        }
-        else if (hourBlock === hour) {
-            $(this).addClass("present")
-        }
-        else {
-            $(this).addClass("future")
-        }
-    })
-
-
 //display local storage text
-function displayItems() {
-    let value = localStorage.getItem(key);
-    if (value) {
-        $(`#text${key}`).text(value);
+text.each(function(){
+        
+    let textStorage = $(this)
+    let storedItem = (textStorage.parent().attr("data-value"))
+
+
+    if (textStorage !== null){
+        var storedDay = localStorage.getItem(storedItem)
+        textStorage.text(storedDay)
     }
-}});
+    else {
+        return
+    }
+
+})
+ //color change dependent on time
+ row.each(function () {
+    let hourBlock = parseInt(this.dataset.value)
+    if (hourBlock < hour) {
+        $(this).addClass("past")
+    }
+    else if (hourBlock == hour) {
+      
+        $(this).addClass("present")
+    }
+    else {
+        $(this).addClass("future")
+    }
+})
+});
 
